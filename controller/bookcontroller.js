7d20de35256502fe5c3a7bb84d3e96203b857eb3
Yakshrajsinh <yakshrajyoyo99@gmail.com>
2025-02-07 14:44:37 +0530

    form added

--- controller/bookcontroller.js
+++ controller/bookcontroller.js
@@ -1,14 +1,12 @@
 const Book=require('../model/bookmodel')
 
 const store=async(req,res)=>{
-    console.log(req.body.book_name);
-    const {book_name,book_price,book_desc}=req.body
+    console.log(req.body);
+    const {book_title,book_price,book_desc,book_auth}=req.body
    await Book.create({
-        book_name,book_price,book_desc
+    book_title,book_price,book_desc,book_auth
     })
-    res.json({
-        messege:'data inserted'
-    })
+    res.redirect("/")
 }
 const index=async(req,res)=>{
     const book=await Book.find()
--- server.js
+++ server.js
@@ -3,17 +3,27 @@ const { default: mongoose } = require('mongoose')
 const app = express()
 const port = 5000
 const Route = require('./Route/bookroute')
+const Book = require('./model/bookmodel')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.set("view engine","ejs")
 
-mongoose.connect("mongodb+srv://Yakshrajsinh:7xfhwJZQ5lz6fzx4@crud.qu43a.mongodb.net/BookStore")
+mongoose.connect("mongodb+srv://Yakshrajsinh:H0vugViHddcRr7pt@crud.qu43a.mongodb.net/BookStore")
     .then(() => {
-        console.log("db connect");
+        console.log("db connect 🙌");
     }).catch((err) => {
         console.log(err);
 
     })
+
+app.get("/",async(req,res)=>{
+    const book = await Book.find() 
+    res.render("pages/form",{
+        book
+    })
+})
+
 app.use('/api/book', Route)
 app.listen(port, () => {
     console.log(`server listening at http://localhost:${port}`);
